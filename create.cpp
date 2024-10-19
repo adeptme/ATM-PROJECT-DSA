e5a5ce2d4c7c88091611d4924c428183c242f278
Ralf Galopo Neis <150760749+RalfNeis@users.noreply.github.com>
2024-10-19 21:27:27 +0800

    recursion

--- create.cpp
+++ create.cpp
@@ -8,6 +8,7 @@
 #include <algorithm>
 #include <windows.h>
 #include <conio.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -18,21 +19,18 @@ struct Account {
    string birthday;
    string contact;
 };
-
 string encrypt(string pin){           //encryption
     for(int i=0; i<pin.size(); i++){
         pin[i] = pin[i] + 200;
     }
     return pin;
 }
-
 string decrypt(string pin){           //decryption
     for(int i=0; i<pin.size(); i++){
         pin[i] = pin[i] - 200;
     }
     return pin;
 }
-
 class create {
     private:
     map<string, Account> accounts;
@@ -57,67 +55,43 @@ class create {
                     pin.pop_back();
                     cout << "\b \b";  // delete the last character from display
                 }
-            }else if(isdigit(ch)){  
-                if(pin.length() < 6){  
+            }else if(isdigit(ch)){
+                if(pin.length() < 6){
                     pin += ch;
-                    cout << '*'; 
+                    cout << '*';
                 }
             }else if(ch == 13){         // enter ASCII code
-                if(pin.length() == 4 || pin.length() == 6){  
-                    break;  
+                if(pin.length() == 4 || pin.length() == 6){
+                    break;
                 }else{
                     cout << "\nInvalid input. PIN must be 4 or 6 digits.\n";
-                    pin.clear();  // Clear the PIN and restart input
+                    pin.clear();
                     cout << "Enter PIN (4 or 6 digits). Press Enter to continue: ";
                 }
             }
-        }
-
-        return pin; 
+        }return pin;
     }
-
+    public:
     bool detectFlashDrive(){
         DWORD fd = GetLogicalDrives();
-        cout << "Flash drive detected: ";
         for(char drive = 'D'; drive <= 'Z'; drive++){
             if(fd & (1 << (drive - 'A'))){
                 string fdpath = string(1, drive) + ":/";
-                
-
-                cout<<fdpath << " "; // debug
-                
-
                 if(GetDriveTypeA(fdpath.c_str()) == DRIVE_REMOVABLE){
+                    system("cls");
                     cout << "\nFlash drive detected at: " << fdpath << endl;
                     drivepath = fdpath + "accounts.txt";
                     return true;
                 }
             }
-        }cout << "\nNo removable flash drive detected."<<endl;
-        return false;
+        }return false;
     }
-
-    public:
-    create(){
-        if(!detectFlashDrive()){
-            cout << "No Flash Drive Detected!" << endl;
-            system("pause");
-        }else{
-            retrieveAccounts();
-        }
-    }
-
     void saveAccounts(){
         if(drivepath.empty()){
             cout << "The Flash Drive is not yet inserted." << endl;
             system("pause");
             return;
         }
-        
-
-        cout << "Saving accounts to:" << drivepath << endl; //debug
-
-        
         ofstream createFile(drivepath);
         if(!createFile){
             cout << "Error in opening the file" << endl;
@@ -125,29 +99,20 @@ class create {
             return;
         }
 
-        
+
         //debug
         if (accounts.empty()){
             cout << "No accounts to save. The accounts map is empty." << endl;
 
             system("pause");
             return;
-        }
-
-        
-        
-        for(const auto& pair: accounts){
-                
-                
+        }for(const auto& pair: accounts){
             //debug
             cout << "Saving account: " << pair.first << " with PIN: " << pair.second.pin << endl;
-            
-            
             createFile << pair.first << " " << encrypt(pair.second.pin) << endl;
         }createFile.close();
         cout << "Accounts saved successfully to: " << drivepath << endl; //debug
     }
-
     void retrieveAccounts(){
         ifstream createFile(drivepath);
         if(!createFile){
@@ -157,16 +122,15 @@ class create {
         }
 
         string accnum, pin;
-        
+
         while(createFile >> accnum >> pin){
             Account acc;
             acc.pin = decrypt(pin);
             accounts[accnum] = acc;
         }createFile.close();
     }
+        void createAccount() {
 
-    void createAccount() {
-        
         string account_number;
 
         do{
@@ -174,8 +138,8 @@ class create {
         }while(accounts.find(account_number) != accounts.end());
 
         Account account;
-        
-        cout << "Enter account holder's name: ";
+
+        cout << "\nEnter account holder's name: ";
         cin >> account.name;
 
         bool valid_balance = false;
@@ -193,25 +157,17 @@ class create {
             }
         }while(!valid_balance);
 
-        account.pin = createPin(); 
+        account.pin = createPin();
 
         accounts[account_number] = account;
         cout << "\nAccount created successfully! \nYour account number is: " << account_number << endl;
-
-        
         //debug
         if(accounts.find(account_number) != accounts.end()){
             cout << "Account added: " << account_number << endl;
         }else{
             cout<<"Account not added." << endl;
-        }
-
-        
-
-        
-        saveAccounts();
+        }saveAccounts();
     }
-
     bool verifyPin(const Account& account) {
         string entered_pin;
         cout << "Enter your 4 or 6-digit PIN: ";
@@ -223,7 +179,7 @@ class create {
             if(ch == 8){                    // backspace ASCII code
                 if(!entered_pin.empty()){
                     entered_pin.pop_back();
-                    cout << "\b \b";  
+                    cout << "\b \b";
                 }
             }else if(isdigit(ch)){
                 if(entered_pin.length() < 6){
@@ -241,13 +197,25 @@ class create {
             }
         }return entered_pin == decrypt(account.pin);
     }
+
+    void idleUSB(create &c) {
+        if (detectFlashDrive() == false) {
+            system("cls");
+            cout << "PLEASE INSERT A FLASH DRIVE." << endl;
+            sleep(1);
+            idleUSB(c);
+    }else{
+        return;
+    }
+}
 };
 
 int main() {
-    
+
     class create c;
     srand(time(0));
 
+    c.idleUSB(c);
     c.createAccount();
 
     return 0;
